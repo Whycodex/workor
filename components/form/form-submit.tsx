754a20963f9894db395fdcc7a320f5bc6024fb60
Whycodex <neerajgupta12300@gmail.com>
2024-06-04 02:33:27 +0530

    Added custom form input and submit

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import { Button } from "../ui/button";
+import { useFormStatus } from "react-dom";
+import { cn } from "@/lib/utils";
+
+interface FormSubmitProps {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
+}
+export const FormSubmit = ({
+  children,
+  disabled,
+  className,
+  variant,
+}: FormSubmitProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      disabled={pending || disabled}
+      type="submit"
+      variant={variant}
+      size="sm"
+      className={cn(className)}
+    >
+      {children}
+    </Button>
+  );
+};

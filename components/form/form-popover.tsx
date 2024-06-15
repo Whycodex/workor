fa1cd0a23dacf81942594ab1980c759d3a2acd1a
Whycodex <neerajgupta12300@gmail.com>
2024-06-16 03:40:12 +0530

    Completed board feature with server actions

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { ElementRef, useRef } from "react";
 
+import { useRouter } from "next/router";
 import { X } from "lucide-react";
 import { toast } from "sonner";
 
@@ -32,13 +33,16 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+  
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log({ data });
       toast.success("Board Created");
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
-      console.log({ error });
       toast.error(error);
     },
   });
@@ -46,7 +50,7 @@ export const FormPopover = ({
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
     const image = formData.get("image") as string;
-    execute({ title });
+    execute({ title, image });
   };
 
   return (
@@ -61,7 +65,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"

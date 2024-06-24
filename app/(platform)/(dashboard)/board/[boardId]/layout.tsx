f26f0fe246f112eb2633d56defaa1f22516a8d00
Whycodex <neerajgupta12300@gmail.com>
2024-06-24 22:55:13 +0530

    Added board id page

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BoardIdLayout = ({children}:{children: React.ReactNode}) => {
+  return (
+    <div className='relative pt-28 h-full'>
+        {children}
+    </div>
+  )
+}
+
+export default BoardIdLayout;
---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BoardIdPage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default BoardIdPage
